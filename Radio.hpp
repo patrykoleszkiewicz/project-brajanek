2f94a934cacb6e9ef1bd6034093b81ca610596f3
patryk <patryk.oleszkiewicz12@gmail.com>
2023-01-24 20:07:18 +0100

    Rework radio

---
+++ Radio.hpp
@@ -0,0 +1,98 @@
+#include <RF24.h>
+#include <SPI.h>
+
+struct DataCar
+{
+    uint16_t rpm1;
+    uint16_t rpm2;
+    uint16_t rpm3;
+    uint16_t rpm4;
+};
+
+struct DataPilot
+{
+    int16_t throttle;
+    int16_t steer;
+    bool reverse;
+    bool brakes;
+};
+
+#if defined(CAR)
+
+const byte addressIn[] = { 0xDE, 0xAD, 0x14, 0x51, 0xDE };
+const byte addressOut[] = { 0x14, 0x51, 0xDE, 0xDE, 0xAD };
+#define CE        9
+#define CSN       10
+#define ERROR_LED 13
+
+#define STOP_TIMEOUT 500
+
+DataCar sentData;
+DataPilot receivedData;
+
+#else
+
+const byte addressIn[] = { 0x14, 0x51, 0xDE, 0xDE, 0xAD };
+const byte addressOut[] = { 0xDE, 0xAD, 0x14, 0x51, 0xDE };
+#define CE        9
+#define CSN       10
+#define ERROR_LED 4
+
+DataPilot sentData;
+DataCar receivedData;
+
+#endif
+
+#define RADIO_TIMEOUT 100
+
+class Radio
+{
+  public:
+    void init()
+    {
+        pinMode(ERROR_LED, OUTPUT);
+
+        if(_radio.begin(CE, CSN))
+        {
+            _radio.openWritingPipe(addressOut);
+            _radio.openReadingPipe(1, addressIn);
+            _radio.setPALevel(RF24_PA_MIN);
+            _radio.setDataRate(RF24_250KBPS);
+        }
+        else
+        {
+            digitalWrite(ERROR_LED, HIGH);
+            while(1) {}
+        }
+    }
+    void update()
+    {
+        if(_transmit)
+        {
+            _radio.stopListening();
+            _radio.write(&sentData, sizeof(sentData));
+            _transmit = false;
+        }
+        else
+        {
+            _radio.startListening();
+            if(_radio.available())
+            {
+                _radio.read(&receivedData, sizeof(receivedData));
+                _transmit = true;
+                receiveTimeout = millis();
+                digitalWrite(ERROR_LED, LOW);
+            }
+            else if(millis() - receiveTimeout > RADIO_TIMEOUT)
+            {
+                _transmit = true;
+                receiveTimeout = millis();
+                digitalWrite(ERROR_LED, HIGH);
+            }
+        }
+    }
+  private:
+    RF24 _radio;
+    bool _transmit;
+    unsigned long receiveTimeout;
+};
--- new_car.ino
+++ new_car.ino
@@ -1,125 +1,27 @@
+#define CAR
+
 #include "Driver.hpp"
-
-#include <RF24.h>
-#include <SPI.h>
-
-#define CE 9
-#define CSN 10
-#define ERROR_LED 13
-
-#define STOP_TIMEOUT  500
-#define RADIO_TIMEOUT 100
-
-RF24 radio(CE, CSN);
-long radio_timer = 0;
-byte cstatus;
-// 0 - OK
-// 1 - NO CONNECTION
-const byte address[] = { 0xDE, 0xAD, 0x14, 0x51, 0xDE };
-
-struct DataSend
-{
-    uint16_t rpm1 = 0;
-    uint16_t rpm2 = 0;
-    uint16_t rpm3 = 0;
-    uint16_t rpm4 = 0;
-    uint8_t s1 = 0;
-    uint8_t s2 = 0;
-    uint8_t s3 = 0;
-    uint8_t s4 = 0;
-    uint8_t s5 = 0;
-    uint8_t s6 = 0;
-    uint8_t s7 = 0;
-    uint16_t bat = 0;
-};
-
-struct DataRecv
-{
-    int16_t throttle = 0;
-    int16_t steer = 0;
-    bool reverse = 0;
-    bool brakes;
-
-    uint8_t control_mode;
-    uint8_t drive_mode;
-    uint8_t steer_comp;
-    uint8_t motor_comp;
-};
-
-DataSend local_state;
-DataRecv remote_state;
-
-long timeoutStop;
+#include "Radio.hpp"
 
 Driver driver;
+Radio radio;
 
-unsigned long currMicros;
-unsigned long currentMillis;
+unsigned long debugTimer;
 
 void setup()
 {
-    pinMode(ERROR_LED, OUTPUT);
     Serial.begin(115200);
 
-    if(radio.begin())
-    {
-        radio.openReadingPipe(0, address);
-        radio.setPALevel(RF24_PA_MAX);
-        radio.setDataRate(RF24_250KBPS);
-        radio.enableDynamicPayloads();
-        radio.enableAckPayload();
-        radio.startListening();
-    }
-    else
-    {
-        digitalWrite(ERROR_LED, HIGH);
-        while(1) {}
-    }
-
+    radio.init();
     driver.init();
 }
 
 void loop()
 {
-    currMicros = micros();
-    currentMillis = millis();
-
-    if(radio.available())
+    radio.update();
+    if(millis() - debugTimer > 200)
     {
-        radio.read(&remote_state, sizeof(remote_state));
-        radio.writeAckPayload(0, &local_state, sizeof(local_state));
-
-        radio_timer = currentMillis;
-        timeoutStop = currentMillis;
-        cstatus = 0;
+        Serial.println(receivedData.steer);
+        debugTimer = millis();
     }
-    else if(currentMillis - radio_timer > RADIO_TIMEOUT)
-    {
-        radio_timer = currentMillis;
-        cstatus = 1;
-
-        radio.begin();
-        radio.openReadingPipe(0, address);
-        radio.setPALevel(RF24_PA_MAX);
-        radio.setDataRate(RF24_250KBPS);
-        radio.enableDynamicPayloads();
-        radio.enableAckPayload();
-        radio.startListening();
-    }
-
-    if(currentMillis - timeoutStop > STOP_TIMEOUT)
-    {
-        //remote_state.throttle = 0;
-    }
-
-    digitalWrite(ERROR_LED, cstatus);
-
-    driver.update(currentMillis, (int)remote_state.throttle * 3, remote_state.steer);
-    //Serial.println(remote_state.throttle);
-    
-    digitalWrite(ERROR_LED, cstatus);
-    //driver.update(currentMillis, 500, 500);
-
-    //Serial.print(" ");
-    //Serial.println(micros() - currMicros);
 }
